d4953cdcd6b930dda1b4c689802b41f35262b645
Jacob Barzee <jbarzee@qualtrics.com>
2018-11-09 20:38:52 -0700

    modified sparse matrix multiply to have two computations and compare the result

--- lab5/sparsematvec.cu
+++ lab5/sparsematvec.cu
@@ -1,62 +1,111 @@
 #include <stdio.h>
-main (int argc, char **argv) {
+
+main(int argc, char **argv)
+{
   FILE *fp;
-  char line[1024]; 
+  char line[1024];
   int *ptr, *indices;
-  float *data, *b, *t;
-  int i,j;
-  int n; // number of nonzero elements in data
+  float *data, *b, *tcpu, *tgpu;
+  int i, j;
+  int n;  // number of nonzero elements in data
   int nr; // number of rows in matrix
   int nc; // number of columns in matrix
 
   // Open input file and read to end of comments
-  if (argc !=2) abort(); 
+  if (argc != 2)
+    abort();
 
-  if ((fp = fopen(argv[1], "r")) == NULL) {
+  if ((fp = fopen(argv[1], "r")) == NULL)
+  {
     abort();
   }
 
   fgets(line, 128, fp);
-  while (line[0] == '%') {
-    fgets(line, 128, fp); 
+  while (line[0] == '%')
+  {
+    fgets(line, 128, fp);
   }
 
   // Read number of rows (nr), number of columns (nc) and
   // number of elements and allocate memory for ptr, indices, data, b and t.
-  sscanf(line,"%d %d %d\n", &nr, &nc, &n);
-  ptr = (int *) malloc ((nr+1)*sizeof(int));
-  indices = (int *) malloc(n*sizeof(int));
-  data = (float *) malloc(n*sizeof(float));
-  b = (float *) malloc(nc*sizeof(float));
-  t = (float *) malloc(nr*sizeof(float));
+  sscanf(line, "%d %d %d\n", &nr, &nc, &n);
+  ptr = (int *)malloc((nr + 1) * sizeof(int));
+  indices = (int *)malloc(n * sizeof(int));
+  data = (float *)malloc(n * sizeof(float));
+  b = (float *)malloc(nc * sizeof(float));
+  tcpu = (float *)malloc(nr * sizeof(float));
+  tgpu = (float *)malloc(nr * sizeof(float));
 
   // Read data in coordinate format and initialize sparse matrix
-  int lastr=0;
-  for (i=0; i<n; i++) {
+  int lastr = 0;
+  for (i = 0; i < n; i++)
+  {
     int r;
-    fscanf(fp,"%d %d %f\n", &r, &(indices[i]), &(data[i]));  
-    indices[i]--;  // start numbering at 0
-    if (r!=lastr) { 
-      ptr[r-1] = i; 
-      lastr = r; 
+    fscanf(fp, "%d %d %f\n", &r, &(indices[i]), &(data[i]));
+    indices[i]--; // start numbering at 0
+    if (r != lastr)
+    {
+      ptr[r - 1] = i;
+      lastr = r;
     }
   }
   ptr[nr] = n;
 
-  // initialize t to 0 and b with random data  
-  for (i=0; i<nr; i++) {
-    t[i] = 0.0;
+  // initialize t to 0 and b with random data
+  for (i = 0; i < nr; i++)
+  {
+    tcpu[i] = 0.0;
+    tgpu[i] = 0.0;
   }
-for (i=0; i<nc; i++) {
-    b[i] = (float) rand()/1111111111;
+  for (i = 0; i < nc; i++)
+  {
+    b[i] = (float)rand() / 1111111111;
   }
 
-  // MAIN COMPUTATION, SEQUENTIAL VERSION
-  for (i=0; i<nr; i++) {                                                      
-    for (j = ptr[i]; j<ptr[i+1]; j++) {
-      t[i] = t[i] + data[j] * b[indices[j]];
+  // create CUDA event handles for timing purposes
+  // cudaEvent_t start_event, stop_event;
+  float elapsed_time_cpu, elapsed_time_gpu;
+
+
+  // Main Computation, CPU version
+  for (i = 0; i < nr; i++)
+  {
+    for (j = ptr[i]; j < ptr[i + 1]; j++)
+    {
+      tcpu[i] = tcpu[i] + data[j] * b[indices[j]];
     }
   }
-  
+
+  // Main Computation, GPU version
+  for (i = 0; i < nr; i++)
+  {
+    for (j = ptr[i]; j < ptr[i + 1]; j++)
+    {
+      tgpu[i] = tgpu[i] + data[j] * b[indices[j]];
+    }
+  }
+
+  // Compare computations to ensure correctness of gpu
+  int res = compare(tcpu, tgpu, nr, 0.001);
+  if (res == 1)
+  {
+    printf("VALID!\n  Sequential Time: %.2f msec\n  Parallel Time: %.2f msec\n Speedup = %.2f\n", elapsed_time_cpu, elapsed_time_gpu, elapsed_time_cpu / elapsed_time_gpu);
+  }
+  else
+  {
+    printf("INVALID...\n");
+  }
+
   // TODO: Compute result on GPU and compare output
+}
+
+int compare(float *a, float *b, int size, double threshold)
+{
+  int i;
+  for (i = 0; i < size; i++)
+  {
+    if (abs(a[i] - b[i]) > threshold)
+      return 0;
+  }
+  return 1;
 }
