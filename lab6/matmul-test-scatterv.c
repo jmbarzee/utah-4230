dd270a2067d6bbbd1ae1f74d70dc5fd4331178c6
Jacob Barzee <jbarzee@qualtrics.com>
2018-12-06 19:49:46 -0700

    first attempt at scatterv

--- lab6/matmul-test-scatterv.c
+++ lab6/matmul-test-scatterv.c
@@ -37,34 +37,26 @@ void main(int argc, char *argv[])
 				b[i][j] = val++;
 			}
 		}
-
-		// int ndims = 2;
-		// int sizes[2] = {N, P};
-		// int subSizes[2] = {SQRP, SQRP};
-		// int starts[2] = {0, 0};
-
-		// MPI_Datatype subArrayA;
-		// MPI_Type_create_subarray(ndims, sizes, subSizes, starts, MPI_ORDER_C, MPI_INT, &subArrayA);
-		// MPI_Type_commit(&subArrayA);
-		// // MPI_Scatterv( sendbuf, scounts, displs, MPI_INT, rbuf, 100, MPI_INT, root, comm);
-
-		// MPI_Datatype subArrayB;
-		// MPI_Type_create_subarray(ndims, sizes, subSizes, starts, MPI_ORDER_C, MPI_INT, &subArrayB);
-		// MPI_Type_commit(&subArrayB);
 	}
-	int rowRank, rowSize;
-	MPI_Comm rowComm;
-	MPI_Comm_split(MPI_COMM_WORLD, rank / (N / SQRP), rank, &rowComm);
-	MPI_Comm_rank(rowComm, &rowRank);
-	MPI_Comm_size(rowComm, &rowSize);
 
-	int colRank, colSize;
-	MPI_Comm colComm;
-	MPI_Comm_split(MPI_COMM_WORLD, rank % (N / SQRP), rank, &colComm);
-	MPI_Comm_rank(colComm, &colRank);
-	MPI_Comm_size(colComm, &colSize);
+	int ndims = 2;
+	int dims[2] = {3, 3};
+	int periods[2] = {1, 1};
+	int reorder = 0;
+	MPI_Comm commCart;
+	MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, commCart);
 
-	printf("WORLD: %d/%d \t ROW: %d/%d \t COL: %d/%d\n", rank, size, rowRank, rowSize, colRank, colSize);
+	int coords[2] = {0, 0} MPI_Cart_coords(commCart, rank, ndims, coords);
+
+	MPI_Datatype block, blocktype;
+	int displacments = {0, 1, 2, 9, 10, 11, 18, 19, 20);
+	int sendCount = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+	int recvCount = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+	MPI_Type_vector(3, 3, 9, MPI_INT, &block);
+	MPI_Type_commit(&block); // not necessary
+	MPI_Type_create_resized(block, 0, 3 * sizeof(int), &blocktype);
+	MPI_Type_commit(&blocktype); // neededint
+	MPI_Scatterv(a, sendCount, displacments, blocktype, mya, recvCount, MPI_Int, 0, commCart);
 
 	// TODO: Scatter 3x3 tiles to mya and myb
 
