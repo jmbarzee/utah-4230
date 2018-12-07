d1ce116edea7b978e3033dfcf23cad3477daef1e
Jacob Barzee <jbarzee@qualtrics.com>
2018-12-06 23:40:51 -0700

    debugging remotely

--- lab6/matmul-assign-cannon.c
+++ lab6/matmul-assign-cannon.c
@@ -71,136 +71,136 @@ void main(int argc, char *argv[])
 		}
 	}
 
-	// // Initilize catersian communicator
-	// int ndims = 2;
-	// int dims[2] = {3, 3};
-	// int periods[2] = {1, 1};
-	// int reorder = 0;
-	// MPI_Comm commCart;
-	// MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &commCart);
+	// Initilize catersian communicator
+	int ndims = 2;
+	int dims[2] = {3, 3};
+	int periods[2] = {1, 1};
+	int reorder = 0;
+	MPI_Comm commCart;
+	MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &commCart);
 
-	// // Grab coordinates of processor
-	// int coords[2] = {0, 0};
-	// MPI_Cart_coords(commCart, rank, ndims, coords);
-	// int x = coords[0];
-	// int y = coords[1];
+	// Grab coordinates of processor
+	int coords[2] = {0, 0};
+	MPI_Cart_coords(commCart, rank, ndims, coords);
+	int x = coords[0];
+	int y = coords[1];
 
-	// // Scatter a and b / Initilize mya and myb
-	// MPI_Datatype block, blocktype;
-	// int displacments[9] = {0, 1, 2, 9, 10, 11, 18, 19, 20};
-	// int sendCount[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-	// int recvCount = 9;
-	// MPI_Type_vector(3, 3, 9, MPI_FLOAT, &block);
-	// MPI_Type_commit(&block); // not necessary
-	// MPI_Type_create_resized(block, 0, 3 * sizeof(float), &blocktype);
-	// MPI_Type_commit(&blocktype); // neededint
-	// MPI_Scatterv(a, sendCount, displacments, blocktype, mya, recvCount, MPI_FLOAT, 0, commCart);
-	// MPI_Scatterv(b, sendCount, displacments, blocktype, myb, recvCount, MPI_FLOAT, 0, commCart);
+	// Scatter a and b / Initilize mya and myb
+	MPI_Datatype block, blocktype;
+	int displacments[9] = {0, 1, 2, 9, 10, 11, 18, 19, 20};
+	int sendCount[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+	int recvCount = 9;
+	MPI_Type_vector(3, 3, 9, MPI_FLOAT, &block);
+	MPI_Type_commit(&block); // not necessary
+	MPI_Type_create_resized(block, 0, 3 * sizeof(float), &blocktype);
+	MPI_Type_commit(&blocktype); // neededint
+	MPI_Scatterv(a, sendCount, displacments, blocktype, mya, recvCount, MPI_FLOAT, 0, commCart);
+	MPI_Scatterv(b, sendCount, displacments, blocktype, myb, recvCount, MPI_FLOAT, 0, commCart);
 
-	// // Initilize myc
-	// for (i = 0; i < SQRP; i++)
-	// {
-	// 	for (j = 0; j < SQRP; j++)
-	// 	{
-	// 		myc[i][j] = 0.0;
-	// 	}
-	// }
+	// Initilize myc
+	for (i = 0; i < SQRP; i++)
+	{
+		for (j = 0; j < SQRP; j++)
+		{
+			myc[i][j] = 0.0;
+		}
+	}
 
-	// // Initialize Send for initial skew of a
-	// int aSendCords[2] = {y, (x - y + 3) % 3};
-	// int aSendRank;
-	// int aSendCount = N * N / P;
-	// MPI_Cart_rank(commCart, aSendCords, &aSendRank);
-	// MPI_Isend(mya, aSendCount, MPI_FLOAT, aSendRank, 0, commCart, &sendreq);
+	// Initialize Send for initial skew of a
+	int aSendCords[2] = {y, (x - y + 3) % 3};
+	int aSendRank;
+	int aSendCount = N * N / P;
+	MPI_Cart_rank(commCart, aSendCords, &aSendRank);
+	MPI_Isend(mya, aSendCount, MPI_FLOAT, aSendRank, 0, commCart, &sendreq);
 
-	// // Initialize Recv for initial skew of a
-	// int aRecvCords[2] = {y, (x + y) % 3};
-	// int aRecvRank;
-	// MPI_Cart_rank(commCart, aRecvCords, &aRecvRank);
-	// MPI_Irecv(mytmp, aSendCount, MPI_FLOAT, aRecvRank, 0, commCart, &rcvreq);
-	// MPI_Wait(&rcvreq, &status);
+	// Initialize Recv for initial skew of a
+	int aRecvCords[2] = {y, (x + y) % 3};
+	int aRecvRank;
+	MPI_Cart_rank(commCart, aRecvCords, &aRecvRank);
+	MPI_Irecv(mytmp, aSendCount, MPI_FLOAT, aRecvRank, 0, commCart, &rcvreq);
+	MPI_Wait(&rcvreq, &status);
 
-	// // Copy mya from mytmp (receive buffer)
-	// for (i = 0; i < SQRP; i++)
-	// {
-	// 	for (j = 0; j < SQRP; j++)
-	// 	{
-	// 		mya[i][j] = mytmp[i][j];
-	// 	}
-	// }
+	// Copy mya from mytmp (receive buffer)
+	for (i = 0; i < SQRP; i++)
+	{
+		for (j = 0; j < SQRP; j++)
+		{
+			mya[i][j] = mytmp[i][j];
+		}
+	}
 
-	// // Initialize Send for initial skew of b
-	// int bSendCords[2] = {(y - x + 3) % 3, x};
-	// int bSendRank;
-	// int bSendCount = N * N / P;
-	// MPI_Cart_rank(commCart, bSendCords, &bSendRank);
-	// MPI_Isend(myb, bSendCount, MPI_FLOAT, bSendRank, 0, commCart, &sendreq);
+	// Initialize Send for initial skew of b
+	int bSendCords[2] = {(y - x + 3) % 3, x};
+	int bSendRank;
+	int bSendCount = N * N / P;
+	MPI_Cart_rank(commCart, bSendCords, &bSendRank);
+	MPI_Isend(myb, bSendCount, MPI_FLOAT, bSendRank, 0, commCart, &sendreq);
 
-	// // Initialize Recv for initial skew of b
-	// int bRecvCords[2] = {(y + x) % 3, x};
-	// int bRecvRank;
-	// MPI_Cart_rank(commCart, bRecvCords, &bRecvRank);
-	// MPI_Irecv(mytmp, bSendCount, MPI_FLOAT, bRecvRank, 0, commCart, &rcvreq);
-	// MPI_Wait(&rcvreq, &status);
+	// Initialize Recv for initial skew of b
+	int bRecvCords[2] = {(y + x) % 3, x};
+	int bRecvRank;
+	MPI_Cart_rank(commCart, bRecvCords, &bRecvRank);
+	MPI_Irecv(mytmp, bSendCount, MPI_FLOAT, bRecvRank, 0, commCart, &rcvreq);
+	MPI_Wait(&rcvreq, &status);
 
-	// // Copy myb from mytmp (receive buffer)
-	// for (i = 0; i < SQRP; i++)
-	// {
-	// 	for (j = 0; j < SQRP; j++)
-	// 	{
-	// 		myb[i][j] = mytmp[i][j];
-	// 	}
-	// }
+	// Copy myb from mytmp (receive buffer)
+	for (i = 0; i < SQRP; i++)
+	{
+		for (j = 0; j < SQRP; j++)
+		{
+			myb[i][j] = mytmp[i][j];
+		}
+	}
 
-	// // TODO: Add following loop:
-	// int q;
-	// for (q = 0; q < SQRP; q++)
-	// {
-	// 	for (i = 0; i < SQRP; i++)
-	// 	{
-	// 		for (j = 0; j < SQRP; j++)
-	// 		{
-	// 			for (k = 0; k < SQRP; k++)
-	// 			{
-	// 				myc[i][j] += mya[i][k] * myb[k][j];
-	// 			}
-	// 		}
-	// 	}
-	// 	// Shift a
-	// 	int aDisplacment = -1;
-	// 	int aDirection = 1; // x dimension
-	// 	int aRankSource, aRankDest;
-	// 	MPI_Cart_shift(commCart, aDirection, aDisplacment, &aRankSource, &aRankDest);
-	// 	MPI_Isend(mya, aSendCount, MPI_FLOAT, aRankDest, 0, commCart, &sendreq);
-	// 	MPI_Irecv(mytmp, aSendCount, MPI_FLOAT, aRankSource, 0, commCart, &rcvreq);
-	// 	MPI_Wait(&rcvreq, &status);
+	// TODO: Add following loop:
+	int q;
+	for (q = 0; q < SQRP; q++)
+	{
+		for (i = 0; i < SQRP; i++)
+		{
+			for (j = 0; j < SQRP; j++)
+			{
+				for (k = 0; k < SQRP; k++)
+				{
+					myc[i][j] += mya[i][k] * myb[k][j];
+				}
+			}
+		}
+		// Shift a
+		int aDisplacment = -1;
+		int aDirection = 1; // x dimension
+		int aRankSource, aRankDest;
+		MPI_Cart_shift(commCart, aDirection, aDisplacment, &aRankSource, &aRankDest);
+		MPI_Isend(mya, aSendCount, MPI_FLOAT, aRankDest, 0, commCart, &sendreq);
+		MPI_Irecv(mytmp, aSendCount, MPI_FLOAT, aRankSource, 0, commCart, &rcvreq);
+		MPI_Wait(&rcvreq, &status);
 
-	// 	// Copy mya from mytmp (receive buffer)
-	// 	for (i = 0; i < SQRP; i++)
-	// 	{
-	// 		for (j = 0; j < SQRP; j++)
-	// 		{
-	// 			mya[i][j] = mytmp[i][j];
-	// 		}
-	// 	}
+		// Copy mya from mytmp (receive buffer)
+		for (i = 0; i < SQRP; i++)
+		{
+			for (j = 0; j < SQRP; j++)
+			{
+				mya[i][j] = mytmp[i][j];
+			}
+		}
 
-	// 	int bDisplacment = -1;
-	// 	int bDirection = 0; // x dimension
-	// 	int bRankSource, bRankDest;
-	// 	MPI_Cart_shift(commCart, bDirection, bDisplacment, &bRankSource, &bRankDest);
-	// 	MPI_Isend(myb, bSendCount, MPI_FLOAT, bRankDest, 0, commCart, &sendreq);
-	// 	MPI_Irecv(mytmp, bSendCount, MPI_FLOAT, bRankSource, 0, commCart, &rcvreq);
-	// 	MPI_Wait(&rcvreq, &status);
+		int bDisplacment = -1;
+		int bDirection = 0; // x dimension
+		int bRankSource, bRankDest;
+		MPI_Cart_shift(commCart, bDirection, bDisplacment, &bRankSource, &bRankDest);
+		MPI_Isend(myb, bSendCount, MPI_FLOAT, bRankDest, 0, commCart, &sendreq);
+		MPI_Irecv(mytmp, bSendCount, MPI_FLOAT, bRankSource, 0, commCart, &rcvreq);
+		MPI_Wait(&rcvreq, &status);
 
-	// 	// Copy mya from mytmp (receive buffer)
-	// 	for (i = 0; i < SQRP; i++)
-	// 	{
-	// 		for (j = 0; j < SQRP; j++)
-	// 		{
-	// 			myb[i][j] = mytmp[i][j];
-	// 		}
-	// 	}
-	// }
+		// Copy mya from mytmp (receive buffer)
+		for (i = 0; i < SQRP; i++)
+		{
+			for (j = 0; j < SQRP; j++)
+			{
+				myb[i][j] = mytmp[i][j];
+			}
+		}
+	}
 
 	// Output local results to compare against sequential
 	for (i = 0; i < SQRP; i++)
