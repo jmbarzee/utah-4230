a77019ee9b85cfe93d06dc94cfe51722ddc49a9e
Jacob Barzee <jbarzee@qualtrics.com>
2018-12-06 22:03:05 -0700

    debugging remotely

--- lab6/matmul-test-scatterv.c
+++ lab6/matmul-test-scatterv.c
@@ -77,38 +77,38 @@ void main(int argc, char *argv[])
 		mya[1][0], myb[1][0], mya[1][1], myb[1][1], mya[1][2], myb[1][2],
 		mya[2][0], myb[2][0], mya[2][1], myb[2][1], mya[2][2], myb[2][2]);
 
-	// // Initialize Send for initial skew of a
-	// int aSendCords[2] = {y, (x - y + 3) % 3};
-	// int aSendRank;
-	// int aSendCount = N * N / P;
-	// MPI_Cart_rank(commCart, aSendCords, &aSendRank);
-	// MPI_Isend(mya, aSendCount, MPI_FLOAT, aSendRank, 0, commCart, &sendreq);
+	// Initialize Send for initial skew of a
+	int aSendCords[2] = {y, (x - y + 3) % 3};
+	int aSendRank;
+	int aSendCount = N * N / P;
+	MPI_Cart_rank(commCart, aSendCords, &aSendRank);
+	MPI_Isend(mya, aSendCount, MPI_FLOAT, aSendRank, 0, commCart, &sendreq);
 
-	// // Initialize Recv for initial skew of a
-	// int aRecvCords[2] = {y, (x + y) % 3};
-	// int aRecvRank;
-	// MPI_Cart_rank(commCart, aRecvCords, &aRecvRank);
-	// MPI_Irecv(mytmp, aSendCount, MPI_FLOAT, aRecvRank, 0, commCart, &rcvreq);
-	// MPI_Wait(&rcvreq, &status);
+	// Initialize Recv for initial skew of a
+	int aRecvCords[2] = {y, (x + y) % 3};
+	int aRecvRank;
+	MPI_Cart_rank(commCart, aRecvCords, &aRecvRank);
+	MPI_Irecv(mytmp, aSendCount, MPI_FLOAT, aRecvRank, 0, commCart, &rcvreq);
+	MPI_Wait(&rcvreq, &status);
 
-	// // Copy mya from mytmp (receive buffer)
-	// for (i = 0; i < SQRP; i++)
-	// {
-	// 	for (j = 0; j < SQRP; j++)
-	// 	{
-	// 		mya[i][j] = mytmp[i][j];
-	// 	}
-	// }
+	// Copy mya from mytmp (receive buffer)
+	for (i = 0; i < SQRP; i++)
+	{
+		for (j = 0; j < SQRP; j++)
+		{
+			mya[i][j] = mytmp[i][j];
+		}
+	}
 
-	// // Dump
-	// printf("postA: %d(%d,%d)\n\
-	// 	(%3.0f, %3.0f) (%3.0f, %3.0f) (%3.0f, %3.0f)\n\
-	// 	(%3.0f, %3.0f) (%3.0f, %3.0f) (%3.0f, %3.0f)\n\
-	// 	(%3.0f, %3.0f) (%3.0f, %3.0f) (%3.0f, %3.0f)\n\n",
-	// 	rank, x, y, 
-	// 	mya[0][0], myb[0][0], mya[0][1], myb[0][1], mya[0][2], myb[0][2],
-	// 	mya[1][0], myb[1][0], mya[1][1], myb[1][1], mya[1][2], myb[1][2],
-	// 	mya[2][0], myb[2][0], mya[2][1], myb[2][1], mya[2][2], myb[2][2]);
+	// Dump
+	printf("postA: %d(%d,%d)\n\
+		(%3.0f, %3.0f) (%3.0f, %3.0f) (%3.0f, %3.0f)\n\
+		(%3.0f, %3.0f) (%3.0f, %3.0f) (%3.0f, %3.0f)\n\
+		(%3.0f, %3.0f) (%3.0f, %3.0f) (%3.0f, %3.0f)\n\n",
+		rank, x, y, 
+		mya[0][0], myb[0][0], mya[0][1], myb[0][1], mya[0][2], myb[0][2],
+		mya[1][0], myb[1][0], mya[1][1], myb[1][1], mya[1][2], myb[1][2],
+		mya[2][0], myb[2][0], mya[2][1], myb[2][1], mya[2][2], myb[2][2]);
 
 	// Initialize Send for initial skew of b
 	int bSendCords[2] = {(y - x + 3) % 3, x};
